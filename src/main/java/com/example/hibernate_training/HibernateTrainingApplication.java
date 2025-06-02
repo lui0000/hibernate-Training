0b0e7ae5a867ecc7e829ef1f78ee8b4827f4a360
Lisa <elbondarenko04@gmail.com>
2025-06-02 17:57:12 +0300

    first commit

---
+++ src/main/java/com/example/hibernate_training/HibernateTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.hibernate_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateTrainingApplication.class, args);
+	}
+
+}
